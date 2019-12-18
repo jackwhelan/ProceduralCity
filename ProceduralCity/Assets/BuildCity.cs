b3d4753f1358d9792307ee681e859ef9387bac28
jackwhelan <jackw97@live.ie>
2019-12-18 17:03:04 +0000

    Added buildcity script
    Generates buildings from an array of buildings at random.

---
+++ ProceduralCity/Assets/BuildCity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildCity : MonoBehaviour
+{
+    public GameObject[] buildings;
+    public int mapWidth = 20;
+    public int mapHeight = 20;
+    public int buildingSpacing = 3;
+
+    void Start()
+    {
+        for(int h = 0; h < mapHeight; h++)
+        {
+            for(int w = 0; w < mapWidth; w++)
+            {
+                Vector3 pos = new Vector3(w * buildingSpacing,0,h * buildingSpacing);
+                int rn = Random.Range(0,buildings.Length);
+                Instantiate(buildings[rn],pos,Quaternion.identity);
+            }
+        }
+    }
+}
